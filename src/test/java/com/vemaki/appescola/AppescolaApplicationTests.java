d20ee8e7150a143d1f7378faede72bf84cb7c8d3
unknown <gersondsnetto@gmail.com>
2020-11-10 18:17:46 -0300

    Projeto Criado

---
+++ src/test/java/com/vemaki/appescola/AppescolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vemaki.appescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppescolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
