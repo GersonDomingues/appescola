b1416cf2e11560187875b1c2710eed7c834c8261
unknown <gersondsnetto@gmail.com>
2020-11-10 19:31:54 -0300

    Testando a primeira classe de dominio - Disciplina

--- src/main/java/com/vemaki/appescola/resources/DisciplinaResource.java
+++ src/main/java/com/vemaki/appescola/resources/DisciplinaResource.java
@@ -1,16 +1,32 @@
 package com.vemaki.appescola.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vemaki.appescola.domain.Disciplina;
 
 @RestController
 @RequestMapping(value="/disciplinas")
 public class DisciplinaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Disciplina> listar() {
+		
+		Disciplina di1 = new Disciplina(1, "Português");
+		Disciplina di2 = new Disciplina(1, "Matemática");
+		
+		List<Disciplina> lista = new ArrayList<>();
+		lista.add(di1);
+		lista.add(di2);
+		
+		return lista;
+		
+		
+		
 	}
 
 }
